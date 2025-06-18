03cd8801dfa435881d7b28bde3a4731e3481e85b
Rahul chhajed <rahulchhajed215@gmail.com>
2025-06-18 13:35:06 +0530

    Update app.js
    updating database name

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 mongoose
-    .connect(`${process.env.MONGO_URI}/fun-tutor`, {
+    .connect(`${process.env.MONGO_URI}/teacher`, {
   useNewUrlParser: true,
   useUnifiedTopology: true
   })
